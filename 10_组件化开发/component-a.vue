6f286c71e29872fee5f76281ddbc6232e3877611
shopkeeper <734212732@qq.com>
2021-08-01 21:59:25 +0800

    第五天

---
+++ 10_组件化开发/component-a.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>{{title}}</h2>
+        <P>{{desc}}</P>
+        <button @click="btnClick">按钮点击</button>
+    </div>
+</template>
+
+    
+
+<script>
+export default {
+    data(){
+        return {
+            title: "我是标题",
+            desc: "我是内容哈哈哈"
+        }
+    },
+    methods: {
+        btnClick(){
+            console.log("按钮发生了点击");
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
