8b6d752d7fedbd442c3de9c84b163c7d1acd8c5b
shopkeeper <734212732@qq.com>
2021-08-06 22:45:28 +0800

    第十天_1

---
+++ learn_webpack/10_vite的基本使用/src/main.js
@@ -0,0 +1,9 @@
+// import _ from 'lodash-es';
+import _ from "../node_modules/lodash-es/lodash.default.js"
+import {sum} from './js/math.js';
+
+
+console.log("hello world");
+console.log(sum(20,30));
+
+console.log(_.join(["abc","cba"],"-"));
