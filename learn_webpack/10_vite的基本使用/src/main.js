8645e83f15e33441f78e7191dccea58a92a8e8d7
CN-Shopkeeper <734212732@qq.com>
2021-08-07 12:56:17 +0800

    第十天

--- learn_webpack/10_vite的基本使用/src/main.js
+++ learn_webpack/10_vite的基本使用/src/main.js
@@ -1,9 +1,24 @@
 // import _ from 'lodash-es';
-import _ from "../node_modules/lodash-es/lodash.default.js"
-import {sum} from './js/math.js';
+import _ from "lodash-es"
+import {sum} from './js/math';
+import mul from './ts/mul'
+import { createApp }  from "vue"
+import App from "./vue/App.vue"
+
+import "./css/style.css";
+import "./css/title.less";
 
 
 console.log("hello world");
 console.log(sum(20,30));
+console.log(mul(20,30));
 
 console.log(_.join(["abc","cba"],"-"));
+
+const titleEl = document.createElement("div");
+titleEl.className= "title";
+titleEl.innerHTML="hello vite";
+document.body.appendChild(titleEl);
+
+// vue
+createApp(App).mount("#app")
---
+++ learn_webpack/10_vite的基本使用/src/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>{{message}}</h2>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message:"Hello App"
+        }
+    }
+}
+</script>
+
+<style>
+    h2{
+        background-color: blueviolet;
+    }
+</style>
