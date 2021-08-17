80854ae61a1c91b7278a22ad799239c3f038fc94
shopkeeper <734212732@qq.com>
2021-08-17 21:48:45 +0800

    第十七天

---
+++ other_component/src/01_render函数的使用/App.vue
@@ -0,0 +1,15 @@
+<script>
+import {h} from 'vue';
+import HelloWorld from './HelloWorld.vue';
+  export default {
+    render() {
+      return h(HelloWorld,{},{
+        default: props=>h("span",null,`App传入HelloWorld的内容: ${props.name}`)
+      });
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ other_component/src/01_render函数的使用/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+import { h } from 'vue';
+    export default {
+        render() {
+            return h("div",null,[
+                h("h2",null,"hello world"),
+                this.$slots.default? this.$slots.default({name:"sk"}):h("span",null,"我是hello world的默认插槽") 
+            ]);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
