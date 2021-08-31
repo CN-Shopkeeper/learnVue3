fa0ad029d409a6aa40507daf1170072b6eb47690
shopkeeper <734212732@qq.com>
2021-08-31 20:44:54 +0800

    view router start

---
+++ learn_vue_router/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <router-link to="/home" replace active-class="home-active">首页</router-link>
+    <router-link to="/about" replace active-class="about-active">关于</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+} 
+</script> 
+
+<style>
+  .home-active{
+    color: red;
+  }
+  .about-active{
+    color: blue;
+  }
+</style>
---
+++ learn_vue_router/src/router/index.js
@@ -0,0 +1,19 @@
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+
+// 配置映射关系
+const routes = [
+  {path: '/', redirect: '/home'},
+  {path: '/home', component: Home},
+  {path: '/about', component: About}
+];
+
+// 创建一个路由对象router
+const router = createRouter({
+  routes,
+  history: createWebHistory()
+})
+
+export default router;
